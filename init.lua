b8eb4cd038d37e3d5e2b72258fd09285908150be
Alex Flores <domain@me.com>
2025-10-11 17:05:07 -0500

    feat(conform): adds conform config

--- init.lua
+++ init.lua
@@ -48,6 +48,29 @@ require("blink.cmp").setup()
 -- })
 -- }}}
 
+-- conform.nvim {{{
+-- TODO autoinstall the formatters with Mason
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "ruff", "isort", "black" },
+		rust = { "rustfmt", lsp_format = "fallback" },
+		javascript = { "deno", stop_after_first = true },
+		go = { "gofumpt", "golines", "goimport-reviser" },
+		sh = { "shfmt" },
+		lisp = { "cljfmt" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
+})
+vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+vim.keymap.set("n", "gm", function()
+	require("conform").format({ lsp_fallback = true, async = false, timeout_ms = 10000 })
+end, { desc = "Format Files" })
+-- }}}
 
 -- nvim-tmux-navigation {{{
 require("nvim-tmux-navigation").setup({
